beb2157bab8a521fb93cbbd16a6af9813a4219e8
zztkdfo <zztkdfo@gmail.com>
2021-10-03 00:31:36 +0900

    JPA AuditableEntity 작업 완료 - domain (createdBy, updatedBy, createdDate, updatedDate)

--- src/main/java/com/ncits/workingdays/domain/AuditableEntity.java
+++ src/main/java/com/ncits/workingdays/domain/AuditableEntity.java
@@ -1,22 +1,24 @@
 package com.ncits.workingdays.domain;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.vavr.control.Option;
+import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.Auditable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Date;
+import java.util.Optional;
 
 @Data
 @Setter(AccessLevel.PROTECTED)
@@ -38,6 +40,72 @@ public abstract class AuditableEntity<ID extends Serializable> implements Audita
 
     @Column(name = "UPDATED_DATE")
     @LastModifiedDate
-    private Date 
+    private Date updatedDate;
+
+    @JsonIgnore
+    @NonNull
+    @Override
+    public Optional<String> getCreatedBy() {
+        return Optional.of(createdBy);
+    }
+
+    @JsonIgnore
+    @Override
+    public void setCreatedBy(@NonNull String createdBy){
+        this.createdBy = createdBy;
+    }
+
+    @JsonIgnore
+    @NonNull
+    @Override
+    public Optional<String> getLastModifiedBy() {
+        return Optional.of(updatedBy);
+    }
+
+    @JsonIgnore
+    @Override
+    public void setLastModifiedBy(@NonNull String lastModifiedBy){
+        this.updatedBy = lastModifiedBy;
+    }
+
+    @JsonIgnore
+    @NonNull
+    @Override
+    public Optional<Instant> getLastModifiedDate() {
+        return Option.of(updatedDate).map(Date::toInstant).toJavaOptional();
+    }
+
+    @JsonIgnore
+    @Override
+    public void setLastModifiedDate(@NonNull Instant lastModifiedDate){
+        this.updatedDate = Option.of(lastModifiedDate).map(Date::from).getOrNull();
+    }
+
+    @JsonIgnore
+    @NonNull
+    @Override
+    public Optional<Instant> getCreatedDate() {
+        return Option.of(createdDate).map(Date::toInstant).toJavaOptional();
+    }
+
+    @JsonIgnore
+    @Override
+    public void setCreatedDate(@NonNull Instant createdDate){
+        this.createdDate = Option.of(createdDate).map(Date::from).getOrNull();
+    }
+
+    @JsonIgnore
+    @Override
+    @Transient
+    public boolean isNew() {
+        return getId() == null;
+    }
+
+    protected AuditableEntity(@Nullable String createdBy, @Nullable String updatedBy, @Nullable Date createdDate, @Nullable Date updatedDate) {
+        this.createdBy = createdBy;
+        this.updatedBy = updatedBy;
+        this.updatedDate = updatedDate;
+        this.createdDate = createdDate;
+    }
 
 }
